533882e20a81950098086cb7a98737aa00f10879
spoorthy23 <32232788+spoorthy23@users.noreply.github.com>
2018-09-17 10:55:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/spoorthy/spoorthyapplication/SpoorthyActivity.java
@@ -0,0 +1,13 @@
+package com.example.spoorthy.spoorthyapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpoorthyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spoorthy);
+    }
+}
